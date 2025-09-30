53296efeca10c8a3cce5975574e7544558e4147b
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-09-30 19:07:32 +0100

    chore: add jobcard component

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,39 +1,98 @@
 import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
-import { assets } from "../assets/assets";
+import {
+  assets,
+  JobCategories,
+  JobLocations,
+  jobsData,
+} from "../assets/assets";
+import JobCard from "./JobCard";
 
 const JobListing = () => {
-  const { isSearched, searchFilter } = useContext(AppContext);
+  const { isSearched, searchFilter, setSearchFilter } = useContext(AppContext);
   return (
-    <div>
+    <div className="container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8">
       {/**side bar */}
 
-      <div>
+      <div className="w-full lg:w-1/4 bg-white px-4">
         {/**search filter from Hero component */}
 
         {isSearched &&
           (searchFilter.title !== "" || searchFilter.location !== "") && (
             <>
-              <h3>Current Search</h3>
+              <h3 className="font-medium text-lg mb-4">Current Search</h3>
 
-              <div>
+              <div className="mb-4 text-gray-600">
                 {searchFilter.title && (
-                  <span>
+                  <span className="inline-flex items-center gap-2.5 bg-blue-50 border border-blue-200 px-4 py-1.5 rounded">
                     {searchFilter.title}
-                    <img className="cursor-pointer" src={assets.cross_icon} />
+                    <img
+                      onClick={(e) =>
+                        setSearchFilter((prev) => ({ ...prev, title: "" }))
+                      }
+                      className="cursor-pointer"
+                      src={assets.cross_icon}
+                    />
                   </span>
                 )}
                 {searchFilter.location && (
-                  <span>
+                  <span className="ml-2 inline-flex items-center gap-2.5 bg-red-50 border border-red-200 px-4 py-1.5 rounded">
                     {searchFilter.location}
 
-                    <img className="cursor-pointer" src={assets.cross_icon} />
+                    <img
+                      onClick={(e) =>
+                        setSearchFilter((prev) => ({ ...prev, location: "" }))
+                      }
+                      className="cursor-pointer"
+                      src={assets.cross_icon}
+                    />
                   </span>
                 )}
               </div>
             </>
           )}
+
+        {/**category Filter */}
+        <div className="max-lg:hidden">
+          <h4 className="font-medium text-lg py-4">Search by Categories</h4>
+          <ul className="space-y-4 text-gray-600">
+            {JobCategories.map((category, index) => (
+              <li className="flex gap-3 items-center" key={index}>
+                <input className="scale-125" type="checkbox" name="" id="" />
+                {category}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/**Location Filter */}
+        <div className="max-lg:hidden">
+          <h4 className="font-medium text-lg py-4 pt-14">Search by Location</h4>
+          <ul className="space-y-4 text-gray-600">
+            {JobLocations.map((location, index) => (
+              <li className="flex gap-3 items-center" key={index}>
+                <input className="scale-125" type="checkbox" name="" id="" />
+                {location}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
+
+      {/**Job Listing */}
+
+      <section className="w-full lg:w-3/4 text-gray-800 max-lg:px-4">
+        <h3 className="font-medium text-3xl py-2" id="job-list">
+          Latest jobs
+        </h3>
+        <p className="mb-8">Get your desired job from top companies</p>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4">
+          {jobsData.map((job, index) => (
+            <JobCard key={index} job={job} />
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
